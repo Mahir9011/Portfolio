b8be2cacd44f8fee78fde377bb16c8c727c659d3
Mahir <92769706+Mahir9011@users.noreply.github.com>
2024-06-02 16:42:08 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,57 @@
+window.PorfolioConfig = {
+  name: "Mahir Daiyan",
+  jobTitle: "Game Developer",
+  jobDescription:
+    "A student trying to escape the matrix",
+  skills: [
+    {
+      name: "Blog Writing",
+      description: "Crafting engaging online content for blogs and websites, requiring research, organization, and clear communication to captivate and inform readers.",
+    },
+    {
+      name: "Web Design",
+      description:
+        "Creating visually appealing and user-friendly websites by designing layouts, choosing color schemes, and selecting typography, aiming to enhance user experience and meet client objectives.",
+    },
+    {
+      name: "Game Development",
+      description: "The process of designing, programming, and creating interactive games for various platforms, such as consoles, computers, and mobile devices, involving elements like storytelling, gameplay mechanics, and visual and audio effects.",
+    },
+  ],
+  works: [
+    {
+      name: "Eid Mubarak page",
+      link: "https://mahir9011.github.io/Eid-Mubarak/",
+      description:
+        "A simple page which takes your name and wishes you a Eid Mubarak",
+    },
+    {
+      name: "ToDo App",
+      link: "https://github.com/Mahir9011/Todo-App",
+      description:
+        "Simple ToDo app made using a python library, Flet",
+    },
+    {
+      name: "Snake Game",
+      link: "https://github.com/Mahir9011/Python-snake-game",
+      description:
+        "Simple Snake game using python library, Pygame",
+    },
+  ],
+  contacts: {
+    links: [
+      {
+        name: "GitHub",
+        link: "https://github.com/Mahir9011/",
+      },
+      {
+        name: "Facebook",
+        link: "https://www.facebook.com/mahir.daiyan.9011",
+      },
+      {
+        name: "Website",
+        link: "http://www.code-matrix.rf.gd",
+      },
+    ],
+  },
+};
